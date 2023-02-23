c2c52bf07e3bcd8571ae84b07c524f474d3f2a1c
jeffreycisneros <jeffreycisneros@localhost>
2023-02-23 12:41:45 -0600

    flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,26 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
-	}
+		StdDraw.clear();
+		StdDraw.rectangle(.5,.5,.4,.3);
+		StdDraw.setPenColor(0,0,150);
+		StdDraw.filledCircle(.5,.5,.15);
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledCircle(.5,.5,.1);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledCircle(.5,.5,.075);
+		StdDraw.setPenColor(0,100,100);
+		double [] x = {.15,.2,.25};
+		double [] y = {.23,.3,.23};
+		StdDraw.filledPolygon(x,y);
+		double [] p = {.15,.2,.25};
+	double [] l = {.77,.7,.77};
+	StdDraw.filledPolygon(p,l);
+	double [] k = {.75,.8,.85};
+	double [] j = {.23,.3,.23};
+	StdDraw.filledPolygon(k,j);
+	double [] o = {.75,.8,.85};
+	double [] m = {.77,.7,.77};
+	StdDraw.filledPolygon(o,m);
+}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,55 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		System.out.print(shape);
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double one = in.nextDouble();
+		double two = in.nextDouble();
+		double three = in.nextDouble();
+		double four = in.nextDouble();
 		
-	}
+		Color colory = new Color(red, green, blue);
+		StdDraw.setPenColor(colory);
+		if (shape.equals("rectangle"))
+		{
+		if (filled == true)
+		{
+			StdDraw.filledRectangle(one, two, three, four);
+		}
+		else {
+			StdDraw.rectangle(one, two, three, four);
+		}
+		}
+		if (shape.equals("ellipse"))	
+		 if (filled == true)
+			{
+				StdDraw.filledEllipse(one, two, three, four);
+			}
+			else {
+				StdDraw.ellipse(one, two, three, four);
+		}
+		if (shape.equals("triangle"))	
+		{
+			double five = in.nextDouble();
+			double six = in.nextDouble();
+			double [] x = {one, three, five};
+			double [] y = {two, four, six};
+			if (filled == true)
+			{
+				StdDraw.filledPolygon(x, y);
+			}
+			else
+			{
+			StdDraw.polygon(x, y);
+			}
+			
+			
+		}
+		}
 }
+	
+
